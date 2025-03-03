ecc4c827a2f84376276692b6938e0ad9d3aafecd
djiax <djiax03@gmail.com>
2025-03-03 13:38:59 +1030

    okok

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function(array,thing) {
-    return array.filter(i=>i!==thing)
+const removeFromArray = function(array,...args) {
+    let newArray=[]
+
+    array.forEach(item=>{
+        if(!args.includes(item)){
+            newArray.push(item)
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
